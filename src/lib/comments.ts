d97fbcefcdf98fdadd6268ef9189cb02686b52d6
sofquipeut <hellosof@gmail.com>
2026-08-20 12:15:34 +0200

    Ajoute la config du système de commentaires (Cusdis)

---
+++ src/lib/comments.ts
@@ -0,0 +1,6 @@
+// Config du système de commentaires, basé sur Cusdis (https://cusdis.com) :
+// léger, gratuit, pas de compte requis pour commenter.
+// Laisser CUSDIS_APP_ID vide désactive complètement le bloc commentaires
+// (voir components/Comments.astro).
+export const CUSDIS_HOST = 'https://cusdis.com';
+export const CUSDIS_APP_ID = '';
