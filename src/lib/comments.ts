71d5c3bc3bf84233cb21dcb1c2de4aa4c59d3329
sofquipeut <hellosof@gmail.com>
2026-08-20 19:29:40 +0100

    Bascule la config commentaires de Cusdis (déprécié) vers Hyvor Talk

--- src/lib/comments.ts
+++ src/lib/comments.ts
@@ -1,6 +1,6 @@
-// Config du système de commentaires, basé sur Cusdis (https://cusdis.com) :
-// léger, gratuit, pas de compte requis pour commenter.
-// Laisser CUSDIS_APP_ID vide désactive complètement le bloc commentaires
+// Config du système de commentaires, basé sur Hyvor Talk (https://talk.hyvor.com) :
+// tier gratuit, compte admin par email classique, posture RGPD.
+// (Remplace Cusdis, dont le projet a été annoncé déprécié le 17 juillet 2026.)
+// Laisser HYVOR_WEBSITE_ID vide désactive complètement le bloc commentaires
 // (voir components/Comments.astro).
-export const CUSDIS_HOST = 'https://cusdis.com';
-export const CUSDIS_APP_ID = '17237820-55e5-47b1-9fa6-05feead59493';
+export const HYVOR_WEBSITE_ID = '';
